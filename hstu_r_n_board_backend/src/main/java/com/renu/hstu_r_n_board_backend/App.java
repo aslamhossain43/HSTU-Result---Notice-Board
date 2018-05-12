6c0b8f17af9ae70b168de98b3cc6af6e150722b4
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-05-12 02:48:51 -0800

    My project setup completed successfully

---
+++ hstu_r_n_board_backend/src/main/java/com/renu/hstu_r_n_board_backend/App.java
@@ -0,0 +1,13 @@
+package com.renu.hstu_r_n_board_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
